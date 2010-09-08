bb15a75cf54c864ecdc87006dcd203bbb18ca79f
Tobias Bielohlawek <tobi@soundcloud.com>
2010-09-08 10:00:52 +0200

    added jeweler integration

--- o2online.rb
+++ o2online.rb
@@ -1,26 +1,58 @@
 require 'rubygems'
 require 'nokogiri'
-require 'firewatir'
 require 'yaml'
 require 'iconv'
 
 require 'ruby-debug'
 
-
 config   = YAML::load(File.open('config.yml'))
 USER     = config["user"]
 PASSWORD = config["password"]
 
-# /Applications/Firefox.app/Contents/MacOS/firefox-bin -jssh
 LOGIN_PAGE  = "https://login.o2online.de/loginRegistration/loginAction.do?_flowId=login&o2_type=asp&o2_label=login/comcenter-login&scheme=http&port=80&server=email.o2online.de&url=%2Fssomanager.osp%3FAPIID%3DAUTH-WEBSSO"
 SENT_PAGE   = "https://email.o2online.de/smscenter_search.osp?SID=124092227_otzulfav&FolderID=0&REF=1283731906&EStart="
 EDIT_PAGE   = "https://email.o2online.de/ssomanager.osp?APIID=AUTH-WEBSSO&TargetApp=/smscenter_new.osp%3FAutocompletion%3D1%26SID%3D124234135_athoonis%26REF%3D1283811721%26MsgContentID%3D"
 
-@browser = FireWatir::Firefox.new
-@browser.goto(LOGIN_PAGE)
-@browser.text_field(:name,"loginName").set(USER)
-@browser.text_field(:name,"password").set(PASSWORD)
-@browser.form(:name, "login").submit
+# /Applications/Firefox.app/Contents/MacOS/firefox-bin -jssh
+# require 'firewatir'
+# @browser = FireWatir::Firefox.new
+# @browser.goto(LOGIN_PAGE)
+# @browser.text_field(:name,"loginName").set(USER)
+# @browser.text_field(:name,"password").set(PASSWORD)
+# @browser.form(:name, "login").submit
+
+require 'mechanize'
+@browser = Mechanize.new { |agent|
+    agent.follow_meta_refresh = true
+  }
+
+@browser.get LOGIN_PAGE
+
+page = @browser.page.form_with(:name => "login") do |form|
+  form.field_with(:name => "loginName").value = USER
+  form.field_with(:name => "password").value = PASSWORD
+end.submit 
+
+#_eventId_login
+
+#@browser.submit(form, form.buttons.last)
+
+
+debugger
+
+@browser.get SENT_PAGE
+
+#search_form.submit
+
+# search_form.submit
+#
+# #search_results = agent.submit(search_form)
+#
+# #agent
+#
+# puts search_results.body
+# #puts agent.page.body
+
 
 def parse_doc( doc )
   doc.css("table.CONTENTLIST tr").each do |elm|
@@ -66,25 +98,3 @@ end
   url = "#{SENT_PAGE}#{i*50}"
   parse_doc html(url)
 end
-
-# agent = Mechanize.new { |a| a.log = Logger.new("mech.log") }
-# agent.get LOGIN_PAGE
-# page = agent.page
-#
-# search_form = page.form_with(:name => "login")
-#
-# puts search_form.has_field?("loginName")
-#
-# search_form.field_with(:name => "loginName").value = USER
-#
-# has_field?(field_name)
-# search_form.field_with(:name => "password").value = PASSWORD
-#
-# search_form.submit
-#
-# #search_results = agent.submit(search_form)
-#
-# #agent.get SENT_PAGE
-#
-# puts search_results.body
-# #puts agent.page.body
