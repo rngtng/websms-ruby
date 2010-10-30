10cb75e2bcffc5160c9d60dd79ae99fe7b9a2b22
Tobias Bielohlawek <tobi@soundcloud.com>
2010-10-30 20:42:00 +0200

    updated config file, added file convert

--- examples/file.rb
+++ examples/file.rb
@@ -5,6 +5,22 @@ require "websms"
 
 config = YAML::load(File.open(File.join(File.dirname(__FILE__),'config.yml')))
 
+def convert(file)
+  output = ""
+  ::File.open(file).each do |line|
+    line.size.times do |i|
+      s = line[i]
+      s = s.strip if s != "\t" && s != " "
+      output << s
+    end
+    output << "\n"
+  end
+
+  ::File.open(file, 'w') do |f|
+    f.puts output
+  end
+end
+
 files = config['file']
 default_cfg = files.delete(:default)
 files.map do |file_name, cfg|
@@ -25,7 +41,9 @@ files.map do |file_name, cfg|
   end
 
   if test_pattern = cfg[:test]
-    size = `grep -e '#{test_pattern}' #{file_name} | wc -l`.strip.to_i
+    size = `grep -E '#{test_pattern}' #{file_name} | wc -l`.strip.to_i
+   # convert("#{file_name}") if size == 1
+    #size = `grep -E '#{test_pattern}' #{file_name} | wc -l`.strip.to_i
   end
 
   print " (#{i}/#{size}) - "
